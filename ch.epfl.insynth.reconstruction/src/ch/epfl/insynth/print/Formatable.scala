4be2bfe5e61f06c0ecb1506829273cc2fb6f62e4
Ivan Kuraj <ivan.kuraj@epfl.ch>
2012-05-30 19:23:48 +0200

    helper class for pretty printing

---
+++ ch.epfl.insynth.reconstruction/src/ch/epfl/insynth/print/Formatable.scala
@@ -0,0 +1,58 @@
+package ch.epfl.insynth.print
+
+import scala.text.Document
+import Document._
+import ch.epfl.insynth.reconstruction.trees._
+import ch.epfl.insynth.env.NormalDeclaration
+
+trait Formatable {
+  def toDocument: Document
+  
+  def println() = {
+	val stdWriter = new java.io.OutputStreamWriter(System.out)
+	this.toDocument.format(140, stdWriter)
+    stdWriter.write('\n')
+    stdWriter.flush()
+  }
+  
+  override def toString = {
+    val stringWriter = new java.io.StringWriter
+	this.toDocument.format(140, stringWriter)
+    stringWriter.flush()    
+    stringWriter.toString
+  }
+}
+
+object FormatHelpers {	
+	
+  def nestedParen(d: Document): Document =
+    group("(" :: nest(1, break :: d) :/: ")")
+	
+  def nestedBrackets(d: Document): Document =
+    group("{" :: nest(1, break :: d) :/: "}")
+
+  def paren(d: Document): Document =
+    group("(" :: d :: ")")
+
+//  def taggedParen2(tag: String, d1: Document, d2: Document) =
+//    taggedParen(tag, d1 :: nest(-tag.length, break :: d2))
+
+  def foldDoc(docs: List[Document], sep: Document): Document = docs match {
+    case Nil => empty
+    case d :: Nil => d
+    case d :: ds => d :: sep :/: foldDoc(ds, sep)
+    case d => throw new RuntimeException("asd" + d)
+  }
+
+  def seqToDoc[T](docs: List[T], sep: String, toDoc: T=>Document): Document =
+    foldDoc(docs map toDoc, sep)
+    
+  def seqToDoc[T](docs: List[T], sep: Document, toDoc: T=>Document): Document =
+    foldDoc(docs map toDoc, sep)
+ 
+  def seqToDoc[T](docs: List[T], toDoc: T=>Document): Document =
+    foldDoc(docs map toDoc, "")
+
+  implicit def strToDoc(s: String): Document = text(s)
+  implicit def intToDoc(i: Int): Document = text(i.toString)
+}
