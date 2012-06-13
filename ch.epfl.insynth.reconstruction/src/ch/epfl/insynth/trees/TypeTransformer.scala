40630aeea2b26b9a61eb2275fdc8b2d79b22856a
Ivan Kuraj <ivan.kuraj@epfl.ch>
2012-06-13 14:26:05 +0200

    TypeTransformer updated

--- ch.epfl.insynth.reconstruction/src/ch/epfl/insynth/trees/TypeTransformer.scala
+++ ch.epfl.insynth.reconstruction/src/ch/epfl/insynth/trees/TypeTransformer.scala
@@ -6,29 +6,18 @@ object TypeTransformer {
 
   //TODO: Cover Variables
   def transform(scalaType:ScalaType):Type = scalaType match {
-    case Method(null, paramss, returnType) 
-		if (paramss != null && returnType != null) => 
-	  transformFunction(returnType, List.flatten(paramss).toSet)
-    case Method(receiver, paramss, returnType)
-    	if (paramss != null && returnType != null) =>
-	  transformFunction(returnType, Set(receiver) ++ List.flatten(paramss).toSet)
-    case Function(params, returnType)
-    	if (params != null && !params.isEmpty && returnType != null) =>
-	  transformFunction(returnType, params.toSet)
-    case ScalaInstance(name, list)
-    	if (list != null && !list.isEmpty && name != null) =>
-	  Instance(name, list.map(transform))
-    case ScalaConst(name) if(name != null) =>
-	  Const(name)
-    case Inheritance(subtype, supertype) if(subtype != null && supertype != null) =>
-      IArrow(TSet(transform(subtype)), transform(supertype))
-    case t =>
-      throw new Exception("Case: "+t.getClass.getName+" should be covered in 'TypeTransformer.transform()'.")
+    case Method(null, paramss, returnType) if (paramss != null && returnType != null) => transformFunction(returnType, paramss.flatten.toSet)
+    case Method(receiver, paramss, returnType) if (paramss != null && returnType != null) => transformFunction(returnType, Set(receiver) ++ paramss.flatten.toSet)
+    case Function(params, returnType) if (params != null && !params.isEmpty && returnType != null) => transformFunction(returnType, params.toSet)
+    case ScalaInstance(name, list) if (list != null && !list.isEmpty && name != null) => Instance(name, list.map(transform))
+    case ScalaConst(name) if(name != null) => Const(name)
+    case Inheritance(subtype, supertype) if(subtype != null && supertype != null)  => IArrow(TSet(transform(subtype)), transform(supertype))
+    case t => throw new Exception("Case: "+t.getClass.getName+" should be covered in 'TypeTransformer.transform()'.")
   }
 
   //TODO: Cover Variables
   private def transformFunction(fun:ScalaType, params:Set[ScalaType]):Type = fun match {
-    case Function(params1, returnType) if (params1 != null && !params.isEmpty && returnType != null) => 
+    case Function(params1, returnType) if (params1 != null && returnType != null) => 
       transformFunction(returnType, params ++ params1)
     case ScalaInstance(name, list) if (list != null && !list.isEmpty && name != null) => 
       if(params.isEmpty) Instance(name, list.map(transform))
