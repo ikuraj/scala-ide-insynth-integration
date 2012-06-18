e42b9ad3ec1386f64466b0e16f589ba260007fe2
Ivan Kuraj <ivan.kuraj@epfl.ch>
2012-06-19 00:08:41 +0200

    reconstructor modified and logging added

--- ch.epfl.insynth.reconstruction/src/ch/epfl/insynth/reconstruction/Reconstructor.scala
+++ ch.epfl.insynth.reconstruction/src/ch/epfl/insynth/reconstruction/Reconstructor.scala
@@ -3,15 +3,28 @@ package ch.epfl.insynth.reconstruction
 import ch.epfl.insynth.env.SimpleNode
 import ch.epfl.insynth.env.Weight
 import ch.epfl.insynth.combinator.Combinator
+import java.util.logging.Logger
 
 object Reconstructor {
 
   def apply(tree:SimpleNode):List[Output] = {
+    // transform the trees (first two steps of the code generation phase)
     val transformedTrees = IntermediateTransformer(Combinator(tree))
        
-    ((transformedTrees.toList map {
+    // for each tree, generate the code for it
+    val generatedCode = transformedTrees.toList map {
       resTree => CodeGenerator(resTree)
-    }).flatten.map {      
+    }
+        
+    // log all snippets
+//    Logger.getLogger("reconstruction.codegen.reconstructor").info(
+//      (generatedCode.flatten.head.toString /: generatedCode.flatten) {
+//        (stringSoFar, formatable) => stringSoFar + "\n" + formatable
+//      } 
+//    )
+    
+    // collect all generated snippets
+    (generatedCode.flatten.map {      
       output => Output(output.toString, new Weight(0.0))
     })    
   }
