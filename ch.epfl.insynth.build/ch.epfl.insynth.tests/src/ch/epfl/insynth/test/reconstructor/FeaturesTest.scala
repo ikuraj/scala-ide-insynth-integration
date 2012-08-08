6ab94ae6b8f995e7c2e00e3aa376d49c9362917f
Ivan Kuraj <ivan.kuraj@epfl.ch>
2012-08-08 15:59:57 +0200

    added missing FeaturesTest

---
+++ ch.epfl.insynth.build/ch.epfl.insynth.tests/src/ch/epfl/insynth/test/reconstructor/FeaturesTest.scala
@@ -0,0 +1,46 @@
+package ch.epfl.insynth.test.reconstructor
+
+import org.junit.Assert._
+import org.junit.Test
+import ch.epfl.insynth.core.Activator
+import ch.epfl.insynth.core.preferences.InSynthConstants
+import ch.epfl.insynth.reconstruction.Reconstructor
+
+class FeaturesTest {
+  
+  @Test
+  def testExample2() {
+    val node = TreeExample.buildSameInSynthDifferentWeight
+    
+    // set appropriate preference values
+		Activator.getDefault.getPreferenceStore.setValue(InSynthConstants.OfferedSnippetsPropertyString, 15)        
+		Activator.getDefault.getPreferenceStore.setValue(InSynthConstants.MaximumTimePropertyString, 500)
+  
+		val reconstructorOutput = Reconstructor(node)
+		
+		// query node has 1.0d weight
+		for ((expectedString, expectedWeight) <- List(("f1(intVal)", 3.0d), ("f2(intVal, intVal)", 4.0d))) {
+      assertTrue(
+        (false /: reconstructorOutput) {
+          (result, output) => {
+            val matchExpected = expectedString
+            output.getSnippet match {
+              case `matchExpected` =>
+                assertEquals(expectedWeight, output.getWieght.getValue, 0d)
+                true
+              case _ => result
+            }
+          }
+        }  
+      )
+    }
+    
+		Activator.getDefault.getPreferenceStore.setValue(InSynthConstants.OfferedSnippetsPropertyString, 1)
+		
+		val reconstructorOutput1Solution = Reconstructor(node)
+		assertTrue(!reconstructorOutput1Solution.isEmpty)
+		assertEquals("f1(intVal)", reconstructorOutput1Solution.head.getSnippet)
+		
+  }
+
+}
