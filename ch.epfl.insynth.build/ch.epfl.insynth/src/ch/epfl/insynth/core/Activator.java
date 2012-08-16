660ba4d5c055d7a360e5fb55b4519bfe5190191e
Ivan Kuraj <ivan.kuraj@epfl.ch>
2012-08-16 21:19:28 +0200

    added InSynth-specific logging

--- ch.epfl.insynth.build/ch.epfl.insynth/src/ch/epfl/insynth/core/Activator.java
+++ ch.epfl.insynth.build/ch.epfl.insynth/src/ch/epfl/insynth/core/Activator.java
@@ -1,8 +1,14 @@
 package ch.epfl.insynth.core;
 
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.SimpleFormatter;
+
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
+
+import ch.epfl.insynth.Config;
 
 /**
  * The activator class controls the plug-in life cycle
@@ -28,6 +34,15 @@ public class Activator extends AbstractUIPlugin {
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
+
+		// sets logger to the InSynth library
+		String inSynthLibraryLoggerFilePath = getStateLocation().toOSString() +
+		  java.io.File.separator + "insynth-library.log";
+		//System.out.println("inSynthLibraryLoggerFilePath: " + inSynthLibraryLoggerFilePath);
+		FileHandler inSynthHandler = new FileHandler(inSynthLibraryLoggerFilePath, true);
+		inSynthHandler.setLevel(Level.ALL);
+		inSynthHandler.setFormatter(new SimpleFormatter());
+		Config.setLoggerHandler(inSynthHandler);
 	}
 
 	/*
