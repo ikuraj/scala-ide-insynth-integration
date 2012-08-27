adbad1b8a13aaea11be8a3c48e34332cad555389
Ivan Kuraj <ivan.kuraj@epfl.ch>
2012-08-27 16:21:55 +0200

    refactored the main test suite

--- ch.epfl.insynth.build/ch.epfl.insynth.tests/src/ch/epfl/insynth/test/TestSuite.scala
+++ ch.epfl.insynth.build/ch.epfl.insynth.tests/src/ch/epfl/insynth/test/TestSuite.scala
@@ -2,18 +2,31 @@ package ch.epfl.insynth.test
 
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
-
-import ch.epfl.insynth.test.completion.InSynthCompletionTests
 import ch.epfl.insynth.test.completion.InSynthPreferencesTests
+import org.junit.BeforeClass
+import ch.epfl.insynth.core.Activator
+import ch.epfl.insynth.core.preferences.InSynthConstants
 
 @RunWith(classOf[Suite])
 @Suite.SuiteClasses(
   Array(
     classOf[InSynthLibraryTestSuite],
     classOf[CodeGenerationTestSuite],
-    classOf[InSynthCompletionTests],
+    classOf[CompletionTestSuite],
     classOf[BenchmarkTestSuite],
     classOf[InSynthPreferencesTests]
   )
 )
 class TestSuite 
+
+object TestSuite {
+  @BeforeClass
+  def setup() {    
+    import InSynthConstants._
+    
+    // set appropriate preference values
+		Activator.getDefault.getPreferenceStore.setValue(OfferedSnippetsPropertyString, 15)        
+		Activator.getDefault.getPreferenceStore.setValue(MaximumTimePropertyString, 500)      
+		Activator.getDefault.getPreferenceStore.setValue(DoSeparateLoggingPropertyString, true)
+  }
+}
