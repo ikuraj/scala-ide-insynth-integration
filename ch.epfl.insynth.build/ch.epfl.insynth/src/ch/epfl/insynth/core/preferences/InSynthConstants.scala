84aff9fef4f3c3e1a38510d56c617a3516415b2c
Ivan Kuraj <ivan.kuraj@epfl.ch>
2012-09-09 21:31:06 +0200

    refactored constants and changed some default values

--- ch.epfl.insynth.build/ch.epfl.insynth/src/ch/epfl/insynth/core/preferences/InSynthConstants.scala
+++ ch.epfl.insynth.build/ch.epfl.insynth/src/ch/epfl/insynth/core/preferences/InSynthConstants.scala
@@ -2,22 +2,22 @@ package ch.epfl.insynth.core.preferences
 
 object InSynthConstants {
   
-  private final val Prefix = "ch.epfl.insynth.properties."
-  final val OfferedSnippetsPropertyString = Prefix + "snippets"  
-  final val MaximumTimePropertyString = Prefix + "maximumTime"  
-  final val DoSeparateLoggingPropertyString = Prefix + "doInSynthLogging"
-  final val CodeStyleParenthesesPropertyString = Prefix + "codeStyleParentheses" 
-  final val CodeStyleApplyOmittingPropertyString = Prefix + "codeStyleApply"    
-  final val CodeStyleSimpleApplicationNameTransformPropertyString = Prefix + "codeStyleSimpleApplicationNameTransform"    
+  private final val Prefix = "ch.epfl.insynth.properties." //$NON-NLS-1$
+  final val OfferedSnippetsPropertyString = Prefix + "snippets" //$NON-NLS-1$
+  final val MaximumTimePropertyString = Prefix + "maximumTime" //$NON-NLS-1$
+  final val DoSeparateLoggingPropertyString = Prefix + "doInSynthLogging" //$NON-NLS-1$
+  final val CodeStyleParenthesesPropertyString = Prefix + "codeStyleParentheses" //$NON-NLS-1$
+  final val CodeStyleApplyOmittingPropertyString = Prefix + "codeStyleApply" //$NON-NLS-1$
+  final val CodeStyleSimpleApplicationNameTransformPropertyString = Prefix + "codeStyleSimpleApplicationNameTransform" //$NON-NLS-1$    
     
-  final val CodeStyleParenthesesClean = "clean"
-  final val CodeStyleParenthesesClassic = "classic"
+  final val CodeStyleParenthesesClean = "clean" //$NON-NLS-1$
+  final val CodeStyleParenthesesClassic = "classic" //$NON-NLS-1$
   
   final val NumberOfOfferedSnippets = 5   
   final val MaximumTime = 500
   final val DoSeparateLogging = false
-  final val CodeStyleParenthesesDefault = CodeStyleParenthesesClean
-  final val CodeStyleApplyOmittingPropertyDefault = false
-  final val CodeStyleSimpleApplicationNameTransformPropertyDefault = false
+  final val CodeStyleParenthesesDefault = CodeStyleParenthesesClassic
+  final val CodeStyleApplyOmittingPropertyDefault = true
+  final val CodeStyleSimpleApplicationNameTransformPropertyDefault = true
 
 }
