642f78c9d2829a98f1a24ac0d18ebbbac9bffe44
kuraj <ivan.kuraj@epfl.ch>
2012-10-03 20:45:23 +0200

    added code extraction to InSynth plugin

---
+++ ch.epfl.insynth.build/ch.epfl.insynth/src/ch/epfl/insynth/leon/Blabla.scala
@@ -0,0 +1,11 @@
+package ch.epfl.insynth.leon
+
+import leon.{ Main => LeonMain }
+import scala.tools.nsc.MainGenericRunner
+import leon.DefaultReporter
+import scala.tools.nsc.{Global,Settings=>NSCSettings,SubComponent,CompilerCommand}
+import leon.Globals
+
+class Blabla {
+
+}
