c302b3bf78664472f8576dfbd4b46098e8826966
Ivan Kuraj <ivan.kuraj@gmail.com>
2016-04-05 15:56:17 -0400

    Add temporary output to read declarations read

--- ch.epfl.insynth.build/ch.epfl.insynth.tests/src/ch/epfl/insynth/test/TestSuite.scala
+++ ch.epfl.insynth.build/ch.epfl.insynth.tests/src/ch/epfl/insynth/test/TestSuite.scala
@@ -7,6 +7,9 @@ import org.junit.BeforeClass
 import ch.epfl.insynth.core.Activator
 import ch.epfl.insynth.core.preferences.InSynthConstants
 
+import ch.epfl.insynth.Config
+
+
 @RunWith(classOf[Suite])
 @Suite.SuiteClasses(
   Array(
@@ -27,5 +30,10 @@ object TestSuite {
 		Activator.getDefault.getPreferenceStore.setValue(OfferedSnippetsPropertyString, 15)        
 		Activator.getDefault.getPreferenceStore.setValue(MaximumTimePropertyString, 500)      
 		Activator.getDefault.getPreferenceStore.setValue(DoSeparateLoggingPropertyString, true)
+		
+		val handler = new java.util.logging.FileHandler("tempOutFile.txt", false)
+		handler.setFormatter(new java.util.logging.SimpleFormatter)	
+		Config.inSynthLogger.setLevel(java.util.logging.Level.FINEST)
+		Config.inSynthLogger.addHandler(handler)
   }
 }
