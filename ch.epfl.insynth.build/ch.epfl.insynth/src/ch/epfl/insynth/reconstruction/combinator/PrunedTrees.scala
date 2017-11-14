e09b292a2e55ca609a3e3f0d41be53b12c687bfa
Fernando Trujano <trujano@mit.edu>
2017-11-14 18:31:31 -0500

    Old files

--- ch.epfl.insynth.build/ch.epfl.insynth/src/ch/epfl/insynth/reconstruction/combinator/PrunedTrees.scala
+++ ch.epfl.insynth.build/ch.epfl.insynth/src/ch/epfl/insynth/reconstruction/combinator/PrunedTrees.scala
@@ -14,7 +14,7 @@ trait Typable {
  * abstract tree node
  */
 abstract class Node(tpe: Type) extends Typable {
-  def getType: Type = tpe  
+  def getType: Type = tpe
 }
 
 case class AbsNode(tpe: Type) extends Node(tpe)
@@ -23,6 +23,54 @@ case class SimpleNode(decls:List[Declaration], tpe: Type, params:Map[Type, Conta
 extends Node(tpe) {
   def getDecls = decls
   def getParams = params
+
+  //Determine if two SimpleNodes are equal
+//  override def equals(that: Any): Boolean = {
+//    println("Checking Node Equality");
+//    that match {
+//      case SimpleNode(tDecls, tTpe, tParams) =>
+//        if (tDecls.size == decls.size) {
+//          var dEqual = false
+//          for (d1 <- decls) {
+//            dEqual = false
+//            for (d2 <- tDecls) {
+//              println("Anbout to call equals")
+//              if (d2 equals d1) dEqual = true
+//              //    	  if (d2.equals(d1)) dEqual = true
+//            }
+//            if (!dEqual) return false
+//          }
+//
+//          val s1Params = params
+//          val s2Params = tParams
+//
+////          return false
+//
+//          //Check that params are the same
+//          //Check that set of keys are equal
+//
+//          println("Decals are equal Checking Params now");
+//          if (s1Params.keys == s2Params.keys) {
+//            //Compare children equality
+//            for ((k, v1) <- s1Params) {
+//              val v2 = s2Params(k)
+//              val s1Nodes = v1.getNodes
+//              val s2Nodes = v2.getNodes
+//              for ((n1, n2) <- (s1Nodes zip s2Nodes)) {
+//                if (!equals(n1, n2)) return false
+//              }
+//            }
+//            return true
+//          }
+//          return false
+//
+//        } else false
+//      case _ =>
+//        false
+//    }
+//
+//  }
+
 }
 
 /**
@@ -31,7 +79,7 @@ extends Node(tpe) {
 case class ContainerNode(var nodes:Set[Node]) {
   def addNode(node:Node) {
     nodes += node
-  }  
+  }
   def getNodes = nodes
 }
 
@@ -41,7 +89,7 @@ object FormatPrNode {
 
 class FormatPrNode(node: Node) extends ch.epfl.insynth.print.Formatable {
   def toDocument = toDocument(node)
-  
+
   def toDocument(node: Any): scala.text.Document = {
     import ch.epfl.insynth.print.FormatHelpers._
 
@@ -51,8 +99,8 @@ class FormatPrNode(node: Node) extends ch.epfl.insynth.print.Formatable {
         "SimpleNode" :: paren(FormatType(tpe).toDocument) :: nestedBrackets(
             seqToDoc(decls, ",", { d:Declaration => strToDoc(d.getSimpleName) })
             :/:
-            seqToDoc(map.toList, ",", 
-              { 
+            seqToDoc(map.toList, ",",
+              {
             	p:(Type, ContainerNode) => paren(FormatType(p._1).toDocument) :: "->" ::
             	nestedBrackets(toDocument(p._2))
               }
